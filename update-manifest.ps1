8b58942a7018790e1255f3e654791755817477a1
Daniele Corsini <franklupog@gmail.com>
2020-10-01 15:45:28 +0200

    Fix missing Functions

--- update-manifest.ps1
+++ update-manifest.ps1
@@ -9,5 +9,6 @@
 # Copyright (C) 2020 Corsinvest Srl	GPLv3 and CEL
 
 $functions = Get-Command -Module Corsinvest.ProxmoxVE.Api -Type Function | Select-Object -ExpandProperty Name
+$functions += 'Build-PveDocumentation', 'Connect-PveCluster', 'ConvertFrom-PveUnixTime', 'ConvertTo-PveUnixTime', 'Find-PveVM'
 $alias = Get-Command -Module Corsinvest.ProxmoxVE.Api -Type Alias | Select-Object -ExpandProperty Name
 Update-ModuleManifest -Path .\Corsinvest.ProxmoxVE.Api\Corsinvest.ProxmoxVE.Api.psd1 -FunctionsToExport $functions -AliasesToExport $alias
