21def8312740263bb8a1d253c4194de60808e277
Daniele Corsini <franklupog@gmail.com>
2022-03-04 09:39:22 +0100

    Improve license

--- publish.ps1
+++ publish.ps1
@@ -1,12 +1,6 @@
-# This file is part of the cv4pve-api-powershell https://github.com/Corsinvest/cv4pve-api-powershell,
-#
-# This source file is available under two different licenses:
-# - GNU General Public License version 3 (GPLv3)
-# - Corsinvest Enterprise License (CEL)
-# Full copyright and license information is available in
-# LICENSE.md which is distributed with this source code.
-#
-# Copyright (C) 2020 Corsinvest Srl	GPLv3 and CEL
+# SPDX-FileCopyrightText: 2022 Daniele Corsini <daniele.corsini@corsinvest.it>
+# SPDX-FileCopyrightText: Copyright Corsinvest Srl
+# SPDX-License-Identifier: GPL-3.0-only
 
 #  $p = [Environment]::GetEnvironmentVariable("PSModulePath")
 #  $path = ";.\Corsinvest.ProxmoxVE.Api";
--- update-manifest.ps1
+++ update-manifest.ps1
@@ -1,12 +1,6 @@
-# This file is part of the cv4pve-api-powershell https://github.com/Corsinvest/cv4pve-api-powershell,
-#
-# This source file is available under two different licenses:
-# - GNU General Public License version 3 (GPLv3)
-# - Corsinvest Enterprise License (CEL)
-# Full copyright and license information is available in
-# LICENSE.md which is distributed with this source code.
-#
-# Copyright (C) 2020 Corsinvest Srl	GPLv3 and CEL
+# SPDX-FileCopyrightText: 2022 Daniele Corsini <daniele.corsini@corsinvest.it>
+# SPDX-FileCopyrightText: Copyright Corsinvest Srl
+# SPDX-License-Identifier: GPL-3.0-only
 
 $functions = Get-Command -Module Corsinvest.ProxmoxVE.Api -Type Function | Select-Object -ExpandProperty Name
 #$functions += 'Connect-PveCluster', 'Invoke-PveRestApi', 'Build-PveDocumentation', 'ConvertTo-PveUnixTime', 'ConvertFrom-PveUnixTime', 'Enter-PveSpice', 'Wait-PveTaskIsFinish', 'Get-PveTaskIsRunning', 'Get-PveStorage', 'Get-PveVM', 'Unlock-PveVM', 'Start-PveVM', 'Stop-PveVM', 'Suspend-PveVM', 'Resume-PveVM', 'Reset-PveVM', 'Get-PveVMSnapshots', 'New-PveVMSnapshot', 'Remove-PveVMSnapshot', 'Undo-PveVMSnapshot'
